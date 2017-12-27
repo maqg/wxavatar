f05895d026cf098a232ad1d8aa72d39f53d5454b
maqg <maquangang@gmail.com>
2017-12-27 14:10:47 +0800

    add images for combine

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,6 +2,13 @@
 //获取应用实例
 const app = getApp()
 
+var pictureList = [
+  "/pages/images/1.png",
+  "/pages/images/2.png",
+  "/pages/images/3.png",
+  "/pages/images/4.png"
+];
+
 Page({
   data: {
     motto: '不疯狂，无人生',
@@ -10,19 +17,42 @@ Page({
     welcome: "欢迎使用北京地铁票价助手",
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
+
   //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  combinePictures: function () {
+    var cv = wx.createCanvasContext("canvas");
+    var len = pictureList.length;
+
+    cv.setFillStyle('red')
+    cv.fillRect(10, 10, 1500, 750)
+    cv.draw()
+
+    function draw(n) {
+      if (n < len) {
+        var img = new Image;
+        img.crossOrigin = 'Anonymous'; //解决跨域  
+        img.src = data[n];
+        console.log(data[n]);
+        img.onload = function () {
+          cv.drawImage(this, 0, 0, 640, 386);
+          draw(n + 1);
+        }
+      } else {
+        base64.push(cv.toDataURL("image/png"));
+        document.getElementById("imgBox").innerHTML = '<image src="' + base64[0] + '">';
+      }
+    }
+    //draw(0)
+
   },
+
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -44,7 +74,7 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
